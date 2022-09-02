4dfe801561742505d3e3b758df65046b7e7f2fbb
З - 6 < - 6@DESKTOP-80ACB9M>
2022-09-02 21:05:03 +0500

    Добавьте файлы проекта.

---
+++ pr2/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refdel {
+    public delegate void DelegatePrint(string a);
+}
+
+ 
---
+++ pr2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+/*void Metod(ref int a) {
+    a++;
+    Console.WriteLine(a);
+}*/
+
+using refdel;
+
+/*void Method2(int a2) {
+    Console.WriteLine("все работает");
+}
+Method refmethod = Method2;
+refmethod(12);*/
+
+void PrintRed(string text) {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine (text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+PrintRed("color");
+
+void PrintBlue(string text) {
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+PrintBlue("color");
+
+void PrintGreen(string text) {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+PrintGreen("color");
+
+void OsnMethod (int p, DelegatePrint DelP) {
+    p = p * p;
+    DelP(p.ToString());
+}
+OsnMethod(12, PrintGreen);
+OsnMethod(122, PrintRed);
+OsnMethod(1222, PrintBlue);
+
+/*void Metodarr(ref int[] arrex) {
+    arrex[2]++;
+}*/
+
+/*int b = 7;
+Metod(ref b);
+Console.WriteLine(b);*/
+
+/*int[] arr = { 1, 2, 3};
+Metodarr(ref arr);
+Console.WriteLine(); */
+
